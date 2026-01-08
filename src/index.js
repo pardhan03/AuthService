ee79916c09e56fecfe39691cf2ffbe20a85af553
Manish Pardhan <pardhanmanish7887@gmail.com>
2026-01-08 23:09:28 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const { PORT } = require('./config/serverConfig');
+console.log(PORT)
+const setupAndStartServer = async () => {
+    const app = express();
+
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({ extended: true }));
+
+    app.listen(PORT, () => {
+        console.log(`Server started at ${PORT}`)
+    });
+}
+
+setupAndStartServer();
