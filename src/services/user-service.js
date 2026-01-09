3afcd2d477b657ac57615a7665458ca2bff0ef70
Manish Pardhan <pardhanmanish7887@gmail.com>
2026-01-09 10:28:55 +0530

    implemented jwt creation and verification

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,7 @@
+const jwt = require('jsonwebtoken');
+
 const { UserRepository } = require('../repository/index');
+const { JWT_KEY } =require('../config/serverConfig');
 
 class UserService {
     constructor() {
@@ -13,6 +16,26 @@ class UserService {
             console.log('Something went wrong in user repo while create:', error);
             throw { error };
         };
+    };
+
+    createToken(user) {
+        try {
+            const result = jwt.sign(user, JWT_KEY, { expiresIn: '1h'});
+            return result;
+        } catch (error) {
+            console.log('Something went wrong at service layer in token creation:', error);
+            throw { error };
+        }
+    };
+
+    verifyToken(token) {
+        try {
+            const result = jwt.verify(token, JWT_KEY);
+            return result;
+        } catch (error) {
+            console.log('Something went wrong at service layer in token verification:', error);
+            throw { error };
+        }
     };
 };
 
